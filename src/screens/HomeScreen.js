9f4398fb49e666645e458655a6dc72121d106a89
Ramrevanth <rkramsaran246@gmail.com>
2022-08-23 23:35:44 +0530

    Adding bootstrap style and rating

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../Components/Product';
 // import logger from 'use-reducer-logger';
 // import data from '../data';
 const reducer = (state, action) => {
@@ -46,22 +49,13 @@ function HomeScreen() {
         ) : error ? (
           { error }
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <strong>
-                    <p>{product.name}</p>
-                  </strong>
-                </Link>
-                <p>${product.price}</p>
-                <button>Add to Cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
