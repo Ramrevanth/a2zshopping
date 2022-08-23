eaca362690034fc15a4bab491edba50e7d1f5314
Ramrevanth <rkramsaran246@gmail.com>
2022-08-23 22:48:52 +0530

    Adding bootstrap style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">a2z shopping</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>a2zshopping</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* <Link to="/">a2z shopping</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-centerd">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
