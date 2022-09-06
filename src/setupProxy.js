71bca6aea0e87902b3f492440e876fa85bbc1d8f
Ramrevanth <rkramsaran246@gmail.com>
2022-09-06 17:51:27 +0530

    proxy update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,17 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
+// import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export default function setupProxy(app) {
+// export default function setupProxy(app) {
+//   app.use(
+//     createProxyMiddleware(['/api', '/otherApi'], {
+//       target: 'http://localhost:5000',
+//     })
+//   );
+// }
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function (app) {
   app.use(
     createProxyMiddleware(['/api', , '/otherApi'], {
       target: 'http://localhost:5000',
     })
   );
-}
+};
